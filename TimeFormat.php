5a038b5a06d7b88e1097ce7c431f18bb07183d0e
shaygan shokrollahi <shayeglin@users.noreply.github.com>
2016-12-29 20:47:29 +0330

    Create TimeFormat.php

---
+++ TimeFormat.php
@@ -0,0 +1,14 @@
+<?php
+function TimeFormat($hour,$format) {
+    switch ($format) {
+        case '12':
+            $hour = $hour - 12;
+            return $hour;
+        break;
+        case '24' :
+            $hour = $hour + 12;
+            return $hour;
+        break;
+    }
+}
+?>
